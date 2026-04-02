342b5efe7fe0587d68309a4e469f3a9a0190e414
Aayushya Shrivastava <152988388+krushayu@users.noreply.github.com>
2026-04-02 18:20:55 +0530

    Fix formatting and add missing committee member

--- src/Pages/Committee.js
+++ src/Pages/Committee.js
@@ -57,7 +57,8 @@ const Committee = () => {
       color: '#96CEB4',
       members: [
         'Prof. (Dr.) Anita Patra, Registrar, CUTM, Odisha',
-        'Prof. (Dr.) Subhransu Ranjan Samantaray, IIT Bhubaneswar, Odisha'
+        'Prof. (Dr.) Subhransu Ranjan Samantaray, IIT Bhubaneswar, Odisha',
+        'Dr. Debendra Kumar Sahoo, Assoc. Professor, Dept. of ECE, CUTM, Odisha'
       ]
     },
     {
@@ -65,8 +66,7 @@ const Committee = () => {
       title: 'Program Chair',
       color: '#FFEAA7',
       members: [
-        'Dr. Prafulla Kumar Panda, Dean SoET (PKD), CUTM, Odisha',
-        'Dr. Debendra Kumar Sahoo, Assoc. Professor, Dept. of ECE, CUTM, Odisha'
+        'Dr. Prafulla Kumar Panda, Dean SoET (PKD), CUTM, Odisha'
       ]
     },
     {
